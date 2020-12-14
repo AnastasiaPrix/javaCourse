75a2b38a6addd79fd724eef8a0b5c6841800538f
AnastasiaPrix <Ohcheeph1234>
2020-12-15 00:42:49 +0300

    lesson 4

--- src/Lesson_4/Main.java
+++ src/Lesson_4/Main.java
@@ -33,7 +33,7 @@ public class Main {
                 System.out.println("DRAW");
                 break;
             }
-            aiStep();
+            aiStep(4);
             printField();
             if (checkWin2(AI_DOT,4)) {
                 System.out.println("WIN SkyNet");
@@ -98,39 +98,95 @@ public class Main {
         setSym(y,x, PLAYER_DOT);
     }
 
-    static void aiStep() {
+    static void aiStep(int countWin) {
         int x,y;
-        do {
-            System.out.println("Ходит SkyNet");
-            x = random.nextInt(SIZE_X);
-            y = random.nextInt(SIZE_Y);
-        } while (!isCellValid(y,x));
-        setSym(y,x, AI_DOT);
-    }
-
-    static boolean checkWinDiag(char sym ,int countWin) {
-        int kD1 = 0;
-        int kD2 = 0;
-        for (int i = 0; i < SIZE_X; i++) {
-            if (field[i][i] == sym) {
-                kD1++;
-            } else if (field[i][i] != sym &&  i >0) {
-                kD1 = 0;
-            }
-            if (field[i][SIZE_Y - i - 1] == sym) {
-                kD2++;
-            } else if (field[i][SIZE_Y - i - 1] != sym &&  i>0) {
-                kD2 = 0;
-            }
-            if (kD1 == countWin|| kD2 == countWin) {
-                return true;
-            }
-
+        System.out.println("Ходит SkyNet");
+        if (!prepareAi(PLAYER_DOT, countWin-2) && !prepareAi(AI_DOT, countWin)) {
+            do {
+                x = random.nextInt(SIZE_X);
+                y = random.nextInt(SIZE_Y);
+            } while (!isCellValid(y,x));
+            setSym(y,x, AI_DOT);
         }
-
-        return false;
     }
-    static boolean checkWinX(char sym ,int countWin) {
+
+    static int checkWinDiag1(char sym ,int countWin) {
+
+        for (int i = 0; i < SIZE_X -1 ; i++) {
+            int kD1 = 0;
+            for (int j = 0; j < SIZE_X - i; j++) {
+                if (field[j+i][j] == sym) {
+                    kD1++;
+                }
+                else if (field[i+j][j] != sym && i ==0 && j>0) {
+                    kD1 = 0;
+                }
+                if (kD1 == countWin) {
+
+                    return j;
+                }
+            }
+        }
+        return -1;
+    }
+    static int checkWinDiag1_2(char sym ,int countWin) {
+
+        for (int i = 0; i < SIZE_X -1 ; i++) {
+            int kD1 = 0;
+            for (int j = 0; j < SIZE_X - i; j++) {
+                if (field[j][j+i] == sym) {
+                    kD1++;
+                }
+                else if (field[j][j+i] != sym && i ==0 && j>0) {
+                    kD1 = 0;
+                }
+                if (kD1 == countWin) {
+
+                    return j;
+                }
+            }
+        }
+        return -1;
+    }
+    static int checkWinDiag2(char sym ,int countWin) {
+
+        for (int i = 0; i < SIZE_X - 1; i++) {
+            int kD2 = 0;
+            for (int j = 0; j < SIZE_Y -i; j++) {
+                if (field[j+i][SIZE_Y - j - 1] == sym) {
+                    kD2++;
+                }
+                if (field[j+i][SIZE_Y - j - 1] != sym && i ==0 && j>0) {
+                    kD2 = 0;
+                }
+                if ( kD2 == countWin) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    static int checkWinDiag2_1(char sym ,int countWin) {
+
+        for (int i = 0; i < SIZE_X - 1; i++) {
+            int kD2 = 0;
+            for (int j = 0; j < SIZE_Y -i; j++) {
+                if (field[j][SIZE_Y - j - 1 - i] == sym) {
+                    kD2++;
+                }
+                if (field[j][SIZE_Y - j - 1 - i] != sym && i ==0 && j>0) {
+                    kD2 = 0;
+                }
+                if ( kD2 == countWin) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    static int checkWinX(char sym ,int countWin) {
         for (int i = 0; i < SIZE_Y; i++) {
             int kX = 0;
             boolean f = true;
@@ -142,14 +198,14 @@ public class Main {
                     kX = 0;
                 }
                 if (kX == countWin) {
-                    return true;
+                    return i;
                 }
             }
 
         }
-        return false;
+        return -1;
     }
-    static boolean checkWinY(char sym, int countWin) {
+    static int checkWinY(char sym, int countWin) {
         for (int i = 0; i < SIZE_X; i++) {
             int kY = 0;
             for (int j = 0; j < SIZE_Y ; j++) {
@@ -160,17 +216,96 @@ public class Main {
                     kY = 0;
                 }
                 if (kY == countWin){
-                    return true;
+                    return i;
                 }
             }
 
         }
+        return -1;
+    }
+
+    static boolean prepareAi (char sym, int countWin) {
+        if (checkWinDiag1(sym, countWin) >=0){
+            aiGoDiag1(checkWinDiag1(sym, countWin));
+            return true;
+        }
+        else if (checkWinDiag2(sym, countWin) >= 0){
+            aiGoDiag2(checkWinDiag2(sym,countWin));
+            return true;
+        }
+        else if (checkWinX(sym,countWin) >=0){
+            aiGoX(checkWinX(sym,countWin));
+            return true;
+        }
+        else if (checkWinY(sym,countWin) >=0){
+            aiGoY(checkWinY(sym,countWin));
+            return true;
+        }
+        else if (checkWinDiag2_1(sym, countWin)>=0){
+            aiGoDiag2_1(checkWinDiag2_1(sym,countWin));
+            return true;
+        }
+        if (checkWinDiag1_2(sym, countWin) >=0){
+            aiGoDiag1_2(checkWinDiag1_2(sym, countWin));
+            return true;
+        }
         return false;
     }
 
-//    Победа для четырех фишек
+    static void aiGoDiag1(int k){
+            for (int j = 0; j < SIZE_X - k; j++) {
+                if (field[j+k][j] == EMPTY_DOT) {
+                    field[j+k][j] = AI_DOT;
+                    break;
+                }
+            }
+    }
+    static void aiGoDiag1_2(int k){
+        for (int j = 0; j < SIZE_X - k; j++) {
+            if (field[j][j+k] == EMPTY_DOT) {
+                field[j][j+k] = AI_DOT;
+                break;
+            }
+        }
+    }
+
+    static void aiGoDiag2(int k){
+        for (int j = 0; j < SIZE_Y -k; j++) {
+            if (field[j+k][SIZE_Y - j - 1] == EMPTY_DOT) {
+                field[j+k][SIZE_Y - j - 1] = AI_DOT;
+                break;
+            }
+        }
+    }
+    static void aiGoDiag2_1(int k){
+        for (int j = 0; j < SIZE_Y -k; j++) {
+            if (field[j][SIZE_Y - j - 1-k] == EMPTY_DOT) {
+                field[j][SIZE_Y - j - 1-k] = AI_DOT;
+                break;
+            }
+        }
+    }
+    static void aiGoY(int k){
+        for (int j = 0; j < SIZE_Y ; j++) {
+            if (field[j][k] == EMPTY_DOT) {
+                field[j][k] = AI_DOT;
+                break;
+            }
+        }
+    }
+    static void aiGoX(int k){
+        for (int j = 0; j < SIZE_Y ; j++) {
+            if (field[k][j] == EMPTY_DOT) {
+                field[k][j] = AI_DOT;
+                break;
+            }
+        }
+    }
+
+
     static boolean checkWin2(char sym, int countWin) {
-        if (checkWinDiag(sym, countWin) || checkWinX(sym, countWin) || checkWinY(sym, countWin)){
+        if (checkWinDiag1(sym, countWin) >= 0 || checkWinX(sym, countWin) >= 0 || checkWinY(sym, countWin) >= 0 || checkWinDiag2(sym, countWin) >= 0
+        || checkWinDiag1_2(sym, countWin) >=0 || checkWinDiag2_1(sym, countWin) >=0){
             return true;
         }
         return false;
